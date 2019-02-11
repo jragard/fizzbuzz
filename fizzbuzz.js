98d07f687692bc0557a6c050a046d99cda4e73bc
Ryan <jragard@gmail.com>
2019-02-11 10:49:28 -0500

    testing

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,7 +2,7 @@ function fizzbuzz(maxValue) {
     let string = "";
     for(let i = 1; i <= maxValue; i++) {
         if(i % 2 == 0 && i % 3 == 0) {
-            string += 'Fizzbuzz,'
+            string += 'FizzBuzz,'
         }
         else if(i % 2 == 0) {
             string += 'Fizz,'
